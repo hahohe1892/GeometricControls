dafa9d2f0c8efa22a6f0b91887ce6b6536495c83
THOMAS <t.frank95@web.de>
2020-11-25 15:21:25 +0100

    retreat ByH900 for another 50 yrs. also changed generate_exp2 file because I thought that was needed for case where shelf extends into bay --> its not, so I undid all changes again (hopefully); GeomProj_extenddomain_SG_spcvx50_NL-450_FrM1200_FlMreal180_FC40_FT50_TS0.01_OF100_hmin350_BuH0_BuP0_BuS0_ByH1800_ByP55000_ByS20000_local_IF60000_MS1000000_MSF200000_xdim85000_accT1325_inH0_smb55_smbPos30000_funnel300_FullMelt_asymmetric; OUTCOME: retreated further, but not at 30 km yet

--- my_Runner2.py
+++ my_Runner2.py
@@ -178,7 +178,7 @@ def extenddomain(params, run_name, load_name, fixfront):
     old_mesh_geometry=md.geometry.bed
     h=np.nan*np.ones(md.mesh.numberofvertices)
     #h[np.where(np.logical_and(np.logical_and(md.mesh.y<17800, md.mesh.y>12200), np.logical_and(md.mesh.x<65000, md.mesh.x>20000)))]=100  #25000
-    h[np.where(np.logical_and(md.geometry.bed<1200, np.logical_and(md.mesh.x<85000, md.mesh.x>30000)))]=100
+    h[np.where(np.logical_and(md.geometry.bed<1200, np.logical_and(md.mesh.x<85000, md.mesh.x>25000)))]=100
     
     md=bamg(md, 'field', old_mesh_geometry, 'hmax', 1000, 'hmin', params['hmin'], 'gradation', 1, 'hVertices', h)
     md.miscellaneous.name=run_name
@@ -284,12 +284,12 @@ def extenddomain(params, run_name, load_name, fixfront):
     #md.geometry.thickness[np.where(md.mesh.x<5)]=md.masstransport.spcthickness[np.where(md.mesh.x<5)]
     #md.geometry.surface[np.where(md.mesh.x<5)]=md.geometry.base[np.where(md.mesh.x<5)]+md.geometry.thickness[np.where(md.mesh.x<5)]
     md.groundingline.melt_interpolation='FullMeltOnPartiallyFloating'
-    md=adddis(md, params)
+#    md=adddis(md, params)
     
     return md
 
 def insertatinflux(params, run_name, load_name, fixfront):
-    restart_time=50
+    restart_time=-1
     inpos=20000
     y_dim, x_dim, slope, dc, gap_halfwidth, step = standardvalues()
     start_icefront=params['start_icefront']
